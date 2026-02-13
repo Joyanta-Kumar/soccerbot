f0e4ca4720cd8339ba7640738d697cc72ea73d13
Joyanta Kumar <joyroy122004@gmail.com>
2026-02-13 21:46:27 +0600

    Started the old soccerbot project so badly without thinking anything.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// [pinA, pinB, pinPWM]
+const int leftMotorPins[3] = { 4, 5, 6 };
+const int rightMotorPins[3] = { 7, 8, 9 };
+
+void pinMode(int pin, int mode) {}
+
+int main()
+{
+  
+  return 0;
+}
